595b5fde6af82e61919ccafc78e4890fee08fdb6
Giovanni Ferrara <giovanni.ferrara@ergo.digital>
2021-02-12 18:40:44 +0100

    add schedule job and checks on products

--- src/index.js
+++ src/index.js
@@ -3,37 +3,57 @@ import axios from 'axios'
 import schedule from 'node-schedule';
 import cheerio from 'cheerio'
 import fs from 'fs'
+import low from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync'
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 let app = express()
-const SPECIAL_PRODUCT = 'five elephant'
-
-const rule = new schedule.RecurrenceRule();
-rule.minute = 0;
-
-const checkIfSpecialProduct = (productsStr, search) => {
-  return productsStr.toLowerCase().includes(search.toLowerCase())
-}
+const URL = 'https://www.coffeedesk.pl/search/five%20elephant/'
 
 const fetchProducts = async ({mock = false}) => {
   let page
   if(mock){
     page = {data: fs.readFileSync('./page.html', {encoding: "utf8", flag:'r'})}
   } else {
-    page = await axios.get('https://www.coffeedesk.pl/search/five%20elephant/')
+    page = await axios.get(URL)
   }
-  const $ = cheerio.load(page.data)
-  return $('.product-title > a')
+  return page.data
 }
 
-const containsSpecialProduct = async () => {
-  const products = await fetchProducts({mock: true})
-  return checkIfSpecialProduct(products, SPECIAL_PRODUCT)
+const getProductsDetails = async () => {
+  const productsRes = await fetchProducts({mock: true})
+  const $ = cheerio.load(productsRes)
+  const unavailable =  $('.product-box').children('.unavailable').length
+  const totalProducts = $('.product-box').length
+  return {availableProducts: totalProducts - unavailable, totalProducts}
 }
 
-(async () => {
-  const res = await containsSpecialProduct()
-  console.log(res)
-})()
+const notify = (text) => {
+  console.log('changed ', text)
+}
+
+const checkForProductUpdates = async () => {
+  console.log('check')
+  const {availableProducts, totalProducts} = await getProductsDetails()
+  const totalProductsPrev = await db.get('totalProducts').value()
+  const availableProductsPrev = await db.get('availableProducts').value()
+
+  if(availableProducts !== availableProductsPrev){
+    await db.set('availableProducts', availableProducts).write()
+    notify('availableProducts')
+  }
+  if(totalProducts !== totalProductsPrev){
+    await db.set('totalProducts', totalProducts).write()
+    notify('totalProducts')
+  }
+}
+
+const rule = new schedule.RecurrenceRule();
+rule.minute = 0;
+
+schedule.scheduleJob('*/5 * * * * *', checkForProductUpdates)
 
 
 let server = app.listen(3000, () => {
