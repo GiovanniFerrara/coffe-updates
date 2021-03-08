4057030344bca0fb291df3fbd330daf92b152e4a
Giovanni Ferrara <giovanni.ferrara@ergo.digital>
2021-03-08 13:07:06 +0100

    format code

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,11 @@
 const info = (obj) => {
-  console.log("info: ", obj ? JSON.stringify(obj, null, 2): "")
-}
+  console.log('info: ', obj ? JSON.stringify(obj, null, 2) : '');
+};
 
 const error = (err) => {
-  console.log("error: ", err)
-}
+  console.log('error: ', err);
+};
 
-const logger = {info, error }
+const logger = { info, error };
 
-export default logger
+export default logger;
